40e0cbb1bd3e5d20bd7786496ad313880aa11e7b
StudenxFinance <studenxfinance@gmail.com>
2024-06-03 19:40:04 +0300

    1

--- src/Model.jsx
+++ src/Model.jsx
@@ -1,96 +1,94 @@
 import React, { useEffect, useRef } from "react";
 import { useGLTF } from "@react-three/drei";
-import gsap from 'gsap'
-import { ScrollTrigger } from 'gsap/ScrollTrigger'
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import { useThree } from "@react-three/fiber";
-import { useLayoutEffect } from "react";
+import { useLayoutEffect, useState } from "react";
 
 export default function Model({ rotate, setRotate, ...props }) {
+  const { camera, scene } = useThree();
+  const model = useRef();
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 800);
 
-  const { camera, scene } = useThree()
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 800);
+    };
 
-  const model = useRef()
+    window.addEventListener('resize', handleResize);
 
-      useEffect(() => {
-        if (rotate) {
-            gsap.to(model.current.rotation, {
-                y: model.current.rotation.y + Math.PI * 2,
-                duration: 2,
-                onComplete: () => setRotate(false),
-            });
-        }
-    }, [rotate, setRotate]);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
 
-  const tl = gsap.timeline()
+  useEffect(() => {
+    if (rotate) {
+      gsap.to(model.current.rotation, {
+        y: model.current.rotation.y + Math.PI * 2,
+        duration: 2,
+        onComplete: () => setRotate(false),
+      });
+    }
+  }, [rotate, setRotate]);
 
-  let mm  = gsap.matchMedia();
+  const tl = gsap.timeline();
+  let mm = gsap.matchMedia();
 
   useLayoutEffect(() => {
-
     mm.add({
       isDesktop: "(min-width: 800px)",
       isMobile: "(max-width: 799px)"
     }, (context) => {
       let { isMobile, isDesktop } = context.conditions;
 
-
-      // FIRST
-
-
       tl
+        .to(".one-title, .one-border", {
+          opacity: 0,
+          yPercent: '-50',
+          scrollTrigger: {
+            trigger: ".two",
+            start: "top bottom",
+            end: "bottom 100%",
+            scrub: true,
+            immediateRender: false,
+          },
+        })
+        .to(model.current.rotation, {
+          x: Math.PI * 2,
+          y: Math.PI * 2,
+          scrollTrigger: {
+            trigger: ".two",
+            start: "top bottom",
+            end: "top top",
+            scrub: true,
+            immediateRender: false,
+          },
+        })
+        .to(".experience", {
+          position: "absolute",
+          scrollTrigger: {
+            trigger: ".three",
+            start: "top bottom",
+            end: "bottom 100%",
+            scrub: true,
+            immediateRender: false,
+          },
+        });
+    });
+  }, []);
 
-      .to(".one-title, .one-border", {
-        opacity: 0,
-        yPercent:'-50',
-        scrollTrigger: {
-          trigger: ".two",
-          start: "top bottom", // Adjust start to start later
-          end: "bottom 100%",
-          scrub: true,
-          immediateRender: false,
-        },
-      })
+  const desktopModelPath = "./desktop.glb";
+  const mobileModelPath = "./mobile.glb";
 
-      .to(model.current.rotation, {
-        x: Math.PI * 2,
-        y: Math.PI * 2,
-        scrollTrigger: {
-          trigger: ".two",
-          start: "top bottom",
-          end: "top top",
-          scrub: true,
-          immediateRender: false,
-        },
-      })
+  const { nodes, materials } = useGLTF(isMobile ? mobileModelPath : desktopModelPath);
 
-      .to(".experience", {
-        position: "absolute",
-        scrollTrigger: {
-          trigger: ".three",
-          start: "top bottom", // Adjust start to start later
-          end: "bottom 100%",
-          scrub: true,
-          immediateRender: false,
-        },
-      })
-      
-
-    })
-    
-
-  }, [])
-
-  const { nodes, materials } = useGLTF("./newcan.glb");
   return (
-    <group {...props} dispose={null} ref={ model } >
-      <mesh
-        castShadow
-        receiveShadow
-        geometry={nodes.can_Baked.geometry}
-        material={materials['can_Baked.004']}
-      />
+    <group {...props} dispose={null} ref={model}>
+      <mesh castShadow receiveShadow geometry={nodes.can.geometry} material={materials.can} />
     </group>
   );
 }
 
-useGLTF.preload("./newcan.glb");
+useGLTF.preload('./desktop.glb')
+useGLTF.preload("./mobile.glb");
