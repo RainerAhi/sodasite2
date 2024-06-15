d94d7722b6c543d2bb05c6bafd55e384ebc6914d
StudenxFinance <studenxfinance@gmail.com>
2024-06-15 10:03:27 +0300

    updates

--- src/Sections/Section5.jsx
+++ src/Sections/Section5.jsx
@@ -73,7 +73,7 @@ export const Section5 = () => {
             <img src="/soda1.JPG" className="soda-small soda-z2" alt="soda1" />
             <img src="/soda2.JPG" className="soda-small" alt="soda2" />
             <img src="/soda5.jpg" className="soda-small" alt="soda3" />
-            <img src="/soda4.jpg" className="soda-small soda-z2" alt="soda4" />
+            <img src="/soda4edited.jpg" className="soda-small soda-z2" alt="soda4" />
             <img src="/soda3.JPG" className="soda-big" alt="soda5" />
           </div>
         </section>
