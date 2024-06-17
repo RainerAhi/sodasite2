4811d580835ef9f950095e05b3de9bad4713fdbb
StudenxFinance <studenxfinance@gmail.com>
2024-06-17 18:08:23 +0300

    ios fix

--- src/Sections/Section6.jsx
+++ src/Sections/Section6.jsx
@@ -8,7 +8,7 @@ export const Section6 = () => {
         <section className="six">
             <div className="six-content">
                 <h1 className="six-text" >LET'S GET READY FOR SUMMER</h1>
-                <video src="/sodavideo.mp4" autoPlay="true" muted="true" playsInline="true" data-wf-ignore="true" preload="metadata" className="background-video" loop ></video>
+                <video src="/sodavideo.mp4" autoPlay="autoplay" muted="true" playsInline="true" data-wf-ignore="true" preload="auto" className="background-video" loop ></video>
             </div>
         </section>
     );
